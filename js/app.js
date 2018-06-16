88b3e37eee42d27ee20fc169dc6d6361621852a9
Diego <dquiroz1988@gmail.com>
2018-06-16 13:18:15 -0400

    3
    formato Botones

--- js/app.js
+++ js/app.js
@@ -21,4 +21,38 @@ var calculadora ={
       x[i].onmouseleave = this.eventoVuelveBoton;
     };
   },
+
+  eventoAchicaBoton: function(event){
+    calculadora.AchicaBoton(event.target);
+  },
+
+  //Formato de Botones
+  AchicaBoton: function(elemento){
+    var x = elemento.id;
+    if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto"){
+      elemento.style.width ="28%";
+      elemento.style.height = "62px";
+    }else if(x=="mas"){
+      elemento.style.width = "88%";
+      elemento.style.height = "98%";
+    }else{
+      elemento.style.width = "21%";
+      elemento.style.height = "62px";
+    }
+  },
+  AumentaBoton: function(elemento){
+    var x = elemento.id;
+    if (x=="1" || x=="2" || x=="3" || x=="0" || x=="igual" || x=="punto"){
+      elemento.style.width ="29%";
+      elemento.style.height = "62.91px";
+    }else if(x=="mas"){
+      elemento.style.width = "90%";
+      elemento.style.height = "100%";
+    }else{
+      elemento.style.width = "22%";
+      elemento.style.height = "62.91px";
+    }
+  },
+
+  
 }
